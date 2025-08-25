4fa22588b524f66c1d79a5d0cc7d6d1d59032ac8
LixdHappy <54619525+LixdHappy@users.noreply.github.com>
2025-08-25 13:11:10 +0800

    Delete functions/api directory

--- functions/api/send.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  return new Response(
-    JSON.stringify({ message: "ok", time: new Date().toISOString() }),
-    {
-      headers: { "Content-Type": "application/json" },
-    }
-  );
-}
